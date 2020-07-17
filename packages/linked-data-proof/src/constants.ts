ab60acbaf526379fada7bcb4ab2599e17455f6da
gjgd <gjgd@protonmail.com>
2020-07-17 11:55:20 +0200

    feat: 1

--- packages/linked-data-proof/src/constants.ts
+++ packages/linked-data-proof/src/constants.ts
@@ -6,4 +6,5 @@ export default {
   SECURITY_CONTEXT_V2_URL: securityConstants.SECURITY_CONTEXT_V2_URL,
   SECURITY_PROOF_URL: 'https://w3id.org/security#proof',
   SECURITY_SIGNATURE_URL: 'https://w3id.org/security#signature',
+  feat: 1
 };
