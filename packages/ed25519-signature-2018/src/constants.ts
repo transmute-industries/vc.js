cdaa0d489bfb5390ed98545884642c798ce18192
gjgd <gjgd@protonmail.com>
2020-07-17 11:23:23 +0200

    fix: test

--- packages/ed25519-signature-2018/src/constants.ts
+++ packages/ed25519-signature-2018/src/constants.ts
@@ -9,7 +9,6 @@ const constants = {
   SECURITY_CONTEXT_V2_URL: securityConstants.SECURITY_CONTEXT_V2_URL,
   SECURITY_PROOF_URL: 'https://w3id.org/security#proof',
   SECURITY_SIGNATURE_URL: 'https://w3id.org/security#signature',
-  TEST: 'CHANGING SOMETHING',
 };
 
 export default constants;
