7bc7a9a90c05a30bd1b227595025ba5e9358dbc6
Orie Steele <orie@transmute.industries>
2021-02-21 13:48:52 -0600

    chore: missing

--- packages/vc.js/src/typings.d.ts
+++ packages/vc.js/src/typings.d.ts
@@ -1,5 +1,3 @@
 declare module 'jsonld';
 declare module 'crypto-ld';
 declare module 'jsonld-signatures';
-declare module '@transmute/did-key-ed25519';
-declare module '@transmute/did-key-bls12381';
