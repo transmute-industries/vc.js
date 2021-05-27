0d61bba28638ed2764c5908714dbaa79d010f513
Adam Carnagey <carnagey.adam@gmail.com>
2021-05-27 15:07:13 -0500

    removing some commented logs

--- packages/vc.js/src/vc-ld/util/credentialUtil.ts
+++ packages/vc.js/src/vc-ld/util/credentialUtil.ts
@@ -4,12 +4,10 @@ const credentialUtil = {
   // format dates the same, don't do ms as it might not be passed
   formatDates: (credential: any) => {
     if (credential.issuanceDate) {
-      // console.log('b', credential.issuanceDate);
       credential.issuanceDate =
         moment(credential.issuanceDate)
           .toISOString()
           .split('.')[0] + 'Z';
-      // console.log('a', credential.issuanceDate);
     }
     if (credential.expirationDate) {
       credential.expirationDate =
